8fb511e68de0d69a66377430ac2b9f45f6242c33
Oskar Mendel <brokenprogrammer@gmail.com>
2017-04-17 16:40:39 +0200

    Updated comments bubble sort

--- src/Sort/BubbleSort.hs
+++ src/Sort/BubbleSort.hs
@@ -29,7 +29,13 @@ bubbleSortiter (x) = (x)
 bubbleSort' :: (Ord a) => [a] -> Int -> [a]
 bubbleSort' xs i = if i == (length xs) then xs else bubbleSort' (bubbleSortiter xs) (i + 1)
 
--- BubbleSort
--- TODO: Add info about bubble sort.
+-- Bubble sort, sometimes referred to as sinking sort, is a simple sorting algorithm
+-- that repeatedly steps through the list to be sorted, compares each pair of adjacent
+-- items and swaps them if they are in the wrong order. The pass through the list is
+-- repeated until no swaps are needed, which indicates that the list is sorted.
+-- The algorithm, which is a comparison sort, is named for the way smaller or
+-- larger elements "bubble" to the top of the list
+--
+-- Worst-case - O(n^2)
 bubbleSort :: (Ord a) => [a] -> [a]
 bubbleSort xs = bubbleSort' xs 0
